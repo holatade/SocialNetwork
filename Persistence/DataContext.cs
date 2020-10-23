f92a4e76894896db3b8d78475220bbdfdee18476
Hassan Hassan <hassan.olatade.hh@gmail.com>
2020-10-23 10:11:41 +0100

    Added basic login functionality using Identity, Jwt token  and user manager

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext(DbContextOptions options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
 
@@ -15,6 +16,7 @@ namespace Persistence
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
             builder.Entity<Value>()
             .HasData(
                 new Value { Id = 1, Name = "Value 101" },
